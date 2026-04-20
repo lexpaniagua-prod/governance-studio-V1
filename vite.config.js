1a6e88ce317aaee8fddcb4cd0c7c0ede5e5e92f3
lexpaniagua-prod <lex.paniagua@aimsos.ai>
2026-04-20 12:51:01 -0500

    Configurar proyecto para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/governance-studio-V1/',
   plugins: [react()],
   resolve: {
     alias: { '@': '/src' }
